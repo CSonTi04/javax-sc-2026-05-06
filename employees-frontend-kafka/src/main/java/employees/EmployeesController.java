9fba7c03038795a4824c0d77710c3910d522d4a3
Tóth András <tibor.andras.toth@idomsoft.hu>
2026-05-06 16:24:47 +0200

    configure Kafka producer with JSON serializer and clean up unused imports in EmployeesController

--- employees-frontend-kafka/src/main/java/employees/EmployeesController.java
+++ employees-frontend-kafka/src/main/java/employees/EmployeesController.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.security.Principal;
 import java.util.HashMap;
 import java.util.Map;
 

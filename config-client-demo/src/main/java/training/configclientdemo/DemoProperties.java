a41b142ded471887a836319c1083c445aba8bb19
Tóth András <tibor.andras.toth@idomsoft.hu>
2026-05-06 12:56:02 +0200

    refactor HelloController to use constructor injection and enable configuration properties

--- config-client-demo/src/main/java/training/configclientdemo/DemoProperties.java
+++ config-client-demo/src/main/java/training/configclientdemo/DemoProperties.java
@@ -1,5 +1,7 @@
 package training.configclientdemo;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
 //demo.prefix néven fogja majd keresni
 @ConfigurationProperties(prefix = "demo")
 public record DemoProperties(String prefix) {
--- config-client-demo/src/main/java/training/configclientdemo/HelloController.java
+++ config-client-demo/src/main/java/training/configclientdemo/HelloController.java
@@ -1,5 +1,11 @@
 package training.configclientdemo;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import java.time.LocalDateTime;
 
 @RestController
@@ -9,10 +15,6 @@ import java.time.LocalDateTime;
 public class HelloController {
 
     private final DemoProperties demoProperties;
-
-    public HelloController(DemoProperties demoProperties) {
-        this.demoProperties = demoProperties;
-    }
 
     @GetMapping
     public String hello() {

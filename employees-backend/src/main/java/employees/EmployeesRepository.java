87c11e4fbc7baaed01a3b056b132228ee63e3e01
Tóth András <tibor.andras.toth@idomsoft.hu>
2026-05-06 14:20:51 +0200

    add projection query to EmployeesRepository for returning DTOs

--- employees-backend/src/main/java/employees/EmployeesRepository.java
+++ employees-backend/src/main/java/employees/EmployeesRepository.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 public interface EmployeesRepository extends JpaRepository<Employee, Long> {
 
+    //projection query -> egyből dto-t adhat vissza
     @Query("select new employees.EmployeeResource(e.id, e.name) from Employee e")
     List<EmployeeResource> findAllResources();
 }

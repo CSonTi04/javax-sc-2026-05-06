857d31a4ebf48aa3d88f6daadea96426b5c6a3b6
Tóth András <tibor.andras.toth@idomsoft.hu>
2026-05-06 16:21:05 +0200

    add new Kafka topics for request and response handling in employee events

--- employees-backend-kafka/src/main/java/employees/KafkaConfiguration.java
+++ employees-backend-kafka/src/main/java/employees/KafkaConfiguration.java
@@ -11,4 +11,14 @@ public class KafkaConfiguration {
     public NewTopic eventTopic() {
         return new NewTopic("employees-bakcend-events", 10, (short) 1);
     }
+
+    @Bean
+    public NewTopic requestTopic() {
+        return new NewTopic("employees-bakcend-request", 10, (short) 1);
+    }
+
+    @Bean
+    public NewTopic responseTopic() {
+        return new NewTopic("employees-bakcend-response", 10, (short) 1);
+    }
 }

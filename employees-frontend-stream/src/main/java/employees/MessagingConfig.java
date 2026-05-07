2f6fcfec4049a4c9005ad0b6bc8c7d3b1d2b3c8b
Tóth András <tibor.andras.toth@idomsoft.hu>
2026-05-07 10:57:20 +0200

    feat: add CreateEmployeeResponse record and configure messaging for employee creation

---
+++ employees-frontend-stream/src/main/java/employees/MessagingConfig.java
@@ -0,0 +1,18 @@
+package employees;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+@Configuration(proxyBeanMethods = false)
+@Slf4j
+public class MessagingConfig {
+
+    @Bean
+    public Consumer<CreateEmployeeResponse> employeeCreated() {
+        return request -> log.info("Employee created: id={}, name={}", request.id(), request.name());
+    }
+}

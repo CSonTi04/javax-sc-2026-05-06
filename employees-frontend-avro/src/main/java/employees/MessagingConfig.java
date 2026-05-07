2d1d1c742afd57ed92e2c7b52133c72461d3bce9
Tóth András <tibor.andras.toth@idomsoft.hu>
2026-05-07 13:27:22 +0200

    feat: configure Avro schema generation and enable schema registry client

--- employees-frontend-avro/src/main/java/employees/MessagingConfig.java
+++ employees-frontend-avro/src/main/java/employees/MessagingConfig.java
@@ -1,12 +1,15 @@
 package employees;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.stream.schema.registry.client.EnableSchemaRegistryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.function.Consumer;
+
 @Configuration(proxyBeanMethods = false)
 @Slf4j
+@EnableSchemaRegistryClient
 public class MessagingConfig {
 
     @Bean

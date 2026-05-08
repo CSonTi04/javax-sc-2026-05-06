7f20ee4e9b67f1eeaf7bb770b88b3bdad3650970
Tóth András <tibor.andras.toth@idomsoft.hu>
2026-05-08 11:21:51 +0200

    feat: Initialize employees oauth FE-BE application with Spring Boot and Maven
    - Add Maven wrapper properties and scripts for project setup
    - Create Dockerfile for containerization of the application
    - Set up pom.xml with Spring Boot dependencies and configurations
    - Implement client configuration for REST communication with backend
    - Define Employee model and EmployeesClient interface for API interactions
    - Create EmployeesController for handling web requests and rendering views
    - Add UserController for user-related functionalities and navigation
    - Configure application properties for backend URL and logging
    - Develop HTML templates for employee listing and creation

---
+++ employees-backend-oauth2/src/main/java/employees/EmployeeResource.java
@@ -0,0 +1,23 @@
+package employees;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+//lehetne rekord!
+//https://jpa-buddy.com/blog/hopefully-the-final-article-about-equals-and-hashcode-for-jpa-entities-with-db-generated-ids/
+@Data
+public class EmployeeResource {
+
+    private Long id;
+
+    @NotBlank
+    private String name;
+
+    public EmployeeResource(String name) {
+        this.name = name;
+    }
+}
---
+++ employees-frontend-oatuh2/src/main/java/employees/ClientConfig.java
@@ -0,0 +1,41 @@
+package employees;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
+import org.springframework.web.client.support.RestClientHttpServiceGroupConfigurer;
+import org.springframework.web.service.invoker.HttpExchangeAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+import org.springframework.web.service.registry.ImportHttpServices;
+
+@Configuration
+@EnableConfigurationProperties(EmployeesProperties.class)
+//meg lehet adni, hogy mit használjon a RestClientHttpServiceGroupConfigurer-ben
+@ImportHttpServices(group = "employees-backend", types = EmployeesClient.class)
+@Slf4j
+public class ClientConfig {
+
+    @Bean
+    RestClientHttpServiceGroupConfigurer groupConfigurer(EmployeesProperties employeesProperties) {
+        log.info("Employees backend url: {}", employeesProperties.getBackendUrl());
+        return groups ->
+            groups.filterByName("employees-backend").forEachClient((group, builder) ->
+                    builder.baseUrl(employeesProperties.getBackendUrl()));
+
+    }
+//    Ez a spring boot 3-mas, ezt váltotta ki a @ImportHttpServices, de így is működik
+//    @Bean
+//    public EmployeesClient employeesClient(RestClient.Builder builder, EmployeesProperties employeesProperties) {
+//        var restClient = builder
+//                .baseUrl(employeesProperties.getBackendUrl())
+//                .build();
+//        var adapter = RestClientAdapter.create(restClient);
+//        var factory = HttpServiceProxyFactory
+//                .builderFor(adapter).build();
+//        return factory.createClient(EmployeesClient.class);
+//    }
+
+}
